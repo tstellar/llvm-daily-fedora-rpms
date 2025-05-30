aaa830e2e0e362a217bca23e98c9865af131f638
Tom Stellard <tstellar@redhat.com>
2025-05-30 23:31:39 +0000

    Fix

--- bisect-with-snapshots.py
+++ bisect-with-snapshots.py
@@ -74,7 +74,7 @@ def test_with_copr_builds(copr_project: str, test_command: str):
     # Enable the copr repo that we want to test.
     # FIXME: There is probably some way to do this via the python API, but I
     # can't figure it out.
-    subprocess.run(f"dnf copr enable -y {copr_fullname}")
+    subprocess.run(["dnf", "copr", "enable", "-y", copr_fullname])
     # Install clang and llvm builds to test
     with dnf.Base() as base:
         base.read_all_repos()
@@ -88,7 +88,7 @@ def test_with_copr_builds(copr_project: str, test_command: str):
     # Disable project so future installs don't use it.
     # FIXME: There is probably some way to do this via the python API, but I
     # can't figure it out.
-    subprocess.run(f"dnf copr disable -y {copr_fullname}")
+    subprocess.run(f["dnf", "copr", "disable", "-y", copr_fullname])
 
     print(test_command)
     #test_command = "git -C /root/llvm-project merge-base --is-ancestor HEAD 6cac792bf9eacb1ed0c80fc7c767fc99c50e252"
