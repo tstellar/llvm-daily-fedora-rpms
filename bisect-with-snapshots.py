02018174ed1105f1275a5293e7fe7d5e11a3e3f7
Tom Stellard <tstellar@redhat.com>
2025-05-31 05:01:06 +0000

    Fix

--- bisect-with-snapshots.py
+++ bisect-with-snapshots.py
@@ -117,7 +117,7 @@ def git_bisect(repo: git.Repo, good_commit: str, bad_commit: str, configure_comm
             fi
             {test_command}
         """
-        subprocess.run(["git", "-C" repo.working_tree_dir, "bisect", "run", "/usr/bin/bash", biscect_script.name)
+        subprocess.run(["git", "-C" repo.working_tree_dir, "bisect", "run", "/usr/bin/bash", biscect_script.name])
     print(repo.git.bisect("log"))
     return True
 
