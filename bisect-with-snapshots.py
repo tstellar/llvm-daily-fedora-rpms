2b66d38647fd6d5718e8eb8e28ecbb24137a59dc
Tom Stellard <tstellar@redhat.com>
2025-06-03 17:35:52 +0000

    Fix

--- bisect-with-snapshots.py
+++ bisect-with-snapshots.py
@@ -110,7 +110,7 @@ def git_bisect(repo: git.Repo, good_commit: str, bad_commit: str, configure_comm
 
     # Use subprocess.run here instead of builtin commands so we can stream output.
     subprocess.run(["git", "-C", repo.working_tree_dir, "bisect", "start", bad_commit, good_commit])
-    with tempfile.NamedTemporaryFile(mode='w+') as bisect_script:
+    with tempfile.NamedTemporaryFile(mode='w+', delete = False) as bisect_script:
         print(f"""
             set -x
             if ! {build_command}; then
